7eb6e3db4a9d9ecd614823152592d60724f43712
David Okeamah <agunnaya001@gmail.com>
2026-04-15 12:10:13 +0100

    Create IUniswapV2Router.sol

---
+++ contracts/interfaces/IUniswapV2Router.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+interface IUniswapV2Router {
+    function addLiquidityETH(
+        address token,
+        uint amountTokenDesired,
+        uint amountTokenMin,
+        uint amountETHMin,
+        address to,
+        uint deadline
+    ) external payable returns (uint, uint, uint);
+}
